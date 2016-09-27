f0fee75f60d1d591c82122679dcce9fa6660efe9
jasper <realshuqian@yahoo.com>
2016-09-27 21:46:28 +0800

    bean map + slf4j + testng reporter

--- src/main/java/com/java/maven/test/example/Person.java
+++ src/main/java/com/java/maven/test/example/Person.java
@@ -1,10 +1,13 @@
 package com.java.maven.test.example;
 
+import java.util.Map;
+
 public class Person {
 	private String name;
 	private int age;
 	public String hometwon;
 	public Person brother;
+	public Map<String,String> pets;
 	/**
 	 * setName
 	 * @param name
@@ -35,8 +38,13 @@ public class Person {
 	public void setBrother(Person brother){
 		this.brother=brother;
 		}	
-	
-	
+	/**
+	 * setPets
+	 * @param pets
+	 */
+	public void setPets(Map<String,String> pets){
+		this.pets=pets;
+		}	
 	
 	/**
 	 * getName
--- src/test/java/com/java/maven/test/example/exampleTest.java
+++ src/test/java/com/java/maven/test/example/exampleTest.java
@@ -1,29 +1,39 @@
 package com.java.maven.test.example;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class exampleTest {
 
+public class exampleTest {
+    static public final Logger logger=LoggerFactory.getLogger(exampleTest.class);
     @BeforeClass
     public void beforeClass() {
         System.out.println("this is before class");
     }
 
     @Test
-    public void TestNgLearn() {
+    public void testSuite() {
         System.out.println("this is TestNG test case");
     	try{
     		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
     			"springModule.xml");
-    	    Person obj = (Person) context.getBean("helloBean",Person.class);
-    	    System.out.println("who is got: "+obj.getName()+" age: "+obj.getAge()+" home: "+obj.hometwon+" brother: "+obj.brother.getName());
-    	    Person obj2 = (Person) context.getBean("secondBean",Person.class);
-    	    System.out.println("who is got: "+obj2.getName()+" age: "+obj2.getAge()+" home: "+obj2.hometwon+" brother: "+obj2.brother.getName());
+    	    Person obj = context.getBean("helloBean",Person.class);
+    	    logger.info("<==== Who is got: {} age: {}; home: {}; brother: {}; pets: {}; ====>",
+    	    		obj.getName(),obj.getAge(),obj.hometwon,obj.brother.getName(),obj.pets);
+    	    Reporter.log("<==== Who is got: "+obj.getName()+" age: "+obj.getAge()+"; home: "+obj.hometwon
+    	    		+"; brother: "+obj.brother.getName()+"; pets: "+obj.pets+"; ====><br />");
+    	    Person obj2 = context.getBean("secondBean",Person.class);
+    	    logger.info("<==== Who is got: {} age: {}; home: {}; brother: {}; pets: {}; ====>",
+    	    		obj2.getName(),obj2.getAge(),obj2.hometwon,obj2.brother.getName(),obj2.pets);
+    	    Reporter.log("<br /><==== Who is got: "+obj2.getName()+" age: "+obj2.getAge()+"; home: "+obj2.hometwon
+    	    		+"; brother: "+obj2.brother.getName()+"; pets: "+obj2.pets+"; ====><br />");
     	    context.close();
     	}catch(Exception e){
     		System.out.println(e.toString());
